91fef13bd716c608038d081a534c96e9cd80c938
exterminathan <exterminathan@gmail.com>
2025-07-06 00:08:39 -0700

    Initial Layout and Images

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,39 @@
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    const scrollPosition = window.scrollY;
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('modeToggle');
 
-    if (scrollPosition > 0) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+  // Init Dark/Light Toggle 
+  toggleButton.textContent = '🌙';
+
+  // D/L Toggle Listener
+  toggleButton.addEventListener('click', () => {
+    const isLight = document.body.classList.toggle('light');
+    toggleButton.textContent = isLight ? '☀️' : '🌙';
+  });
+
+  // Toggle Main Project
+  const mainContainer = document.querySelector('.main-project-container');
+  const mainDetail    = document.getElementById('main-project');
+  mainContainer.addEventListener('click', () => {
+    mainDetail.classList.toggle('visible');
+  });
+
+  // Toggle Other Projects
+  document.querySelectorAll('.project-row').forEach(row => {
+    row.addEventListener('click', () => {
+      const detail = document.getElementById(row.dataset.target);
+      detail.classList.toggle('visible');
+    });
+  });
+
+  // Phone Popup Toggle 
+    const phoneIcon = document.querySelector('.phone-icon');
+    const phonePopup = phoneIcon.querySelector('.phone-popup');
+
+    phoneIcon.addEventListener('click', () => {
+    phonePopup.classList.toggle('visible');
+    });
+
 });
+
+
+
